dc5b71f7bf3ce5d73c85b7973921a73aadc32823
alffox <alfonso.crisci.it@gmail.com>
2017-11-30 11:56:50 +0100

    bug: Define player

--- js/app.js
+++ js/app.js
@@ -24,7 +24,9 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -33,7 +35,7 @@ var ladybug1 = new Enemy(50, 200, 0);
 var ladybug2 = new Enemy(50, 300, 0);
 var ladybug3 = new Enemy(55, 250, 0);
 var allEnemies = [ladybug1, ladybug2, ladybug3];
-
+var player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
